526cc2d3b94b3803dd345c3b7466c72350940811
MereacreEugen01 <mereacre.eugen@studenti-ittfedifermi.edu.it>
2022-04-09 21:04:39 +0000

    Cominciato primo abbozzo di API. Fatto il Router con una sola funzione al momento ma manca la restituzione delle risposte in JSON

--- api.php
+++ api.php
@@ -1,8 +1,55 @@
 <?php 
+
+function classResponde($status = 200, $body = '', $conten_type = 'text/html')
+{
+    //non ho ben capito sta parte
+}
+
+
 class serviceApi
 {
+    private $db_connection;
     //metodi
+    function __construct()
+    {
+        $db_connection = new mysqli("localhost","Eugen","120901");
+        if ($db_connection->connect_error)
+        {
+            die("conn failed: ". $db_connection->connect_error);
+        }
+        else
+        {
+            //echo "Connessione riuscita";
+        }
+    }
+    function __destruct()
+    {
+
+    }
+
+    function elencaMulte()
+    {
+        echo "Hai chiesto di elencare le multe";
+    }
+    
 }
 //instanziamo oggetto Api
 $api = new serviceApi();
+$nome_funzione = $_POST['function'];
+//echo "Richiesta fatta: "."$nome_funzione";
+//router
+switch($nome_funzione)
+{
+    case 'elencaMulte':
+       $api->elencaMulte();
+    break;
+
+    default: 
+    echo "C'è stato un problema ci scusiamo";
+    break;
+}
+
+
+
+
 ?>
