9160ab1d5d455033393e08c72d66df06d5d593bb
Eugen Mereacre <mereacre.eugen@studenti-ittfedifermi.edu.it>
2022-04-24 15:10:27 +0000

    Proseguimento con la creazione dell'API, mancano alcuni accorgiento come la plularità delle sanzioni e cancellare più multe insieme

--- api.php
+++ api.php
@@ -1,7 +1,5 @@
 <?php 
 
-
-
 function sendResponse($status = 200, $body = '', $conten_type = 'text/html')
 {
     $HTTPHeader = 'HTTP/1.1 '.$status.' '.'UNKNOWN';
@@ -11,7 +9,6 @@ function sendResponse($status = 200, $body = '', $conten_type = 'text/html')
     header('Content-type: '.$conten_type);
     //Scrivo nel body della risposta
     echo $body; 
-
 }
 
 $autenticato = false;
@@ -19,9 +16,6 @@ $autenticato = false;
 class serviceApi
 {
     private $db_connection;
-    
-
-    
     //metodi
     function __construct()
     {
@@ -35,7 +29,6 @@ class serviceApi
         try
         {
             $this->$db_connection = new PDO($dsn,$username,$password);
-
         }
         catch(PDOException $e)
         {
@@ -51,50 +44,133 @@ class serviceApi
     function nuovaMulta()
     {
         //echo "Hai chiesto di fare una nuova multa";
+        $targa = $_POST['targa'];
+        $matricola = $_POST['operatore'];
+        $effrazione[] = $_POST['effrazione'];
+        $luogo = $_POST['luogo'];
+        $importo = $_POST['importo'];
+        $foto = $_POST['foto'];
+        $longitudine = $_POST['longitudine'];
+        $latitudine = $_POST['latitudine'];
+        try
+        {
+            $miaQuery = "INSERT INTO multe VALUES (NULL, '$matricola', DEFAULT, '$luogo','$importo','$foto','$longitudine','$latitudine')";
+            //echo $miaQuery; 
+           ///// $statement = $this->$db_connection->query($miaQuery); // in questo modo funziona ma non è sicuro per l'sql injection
+            /*  non funziona
+            $statement = $this->db_connection->prepare("INSERT INTO multe VALUES (NULL,:id_operatore,DEFAULT,:luogo,:importo,:foto,:longitudine,:latitudine)");
+            $statement->excetute([':id:operatore' => $matricola, ':luogo' => $luogo, ':importo' => $importo, ':foto' => $foto, ':longitudine' => $longitudine, ':latitudine' => $latitudine]);
+            */
+            //$statement->execute( [ $matricola,  $luogo,$importo,$foto,$longitudine,$latitudine]);
+            //sendResponse(200, json_encode($statement), "application/json");
+            //echo "funziona";
+
+            //per farsi restituire l'ultima multa inserita 
+           //////////////// $miaQuery = "SELECT * FROM multe WHERE ID_Multa = ( SELECT max(ID_Multa) FROM multe )";
+            ///////////////$statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
+           /////////////// $risultati = $statement->fetchAll();
+           ///////////////// $vettoreIdOttenuto = $risultati[0];
+            //prendo il valore del token e lo metto in una variabile di supporto 
+            //per poter verificare se il token è prensente nel database
+           /////////////// $IdOttenuto = $vettoreIdOttenuto['ID_Multa'];
+           // echo $IdOttenuto;
+          // echo json_encode($_POST['effrazione']);
+          /*
+            $arrAssco = [
+                'nome' => 'luca',
+                'cognome' => 'rossi',
+                'eta' => 25
+            ];
+            foreach($arrAssco as $chiave => $valore)
+            {
+                echo $chiave.$valore;
+            }
+           */
+            
+
+        /*
+            $statement = $this->db_connection->prepare("INSERT INTO libri VALUES (NULL,?,?,?)");
+
+            //$statement->execute([':titolo' => $_GET['titolo'], ':autore'=> $_GET['autore'], ':prezzo' => $_GET['prezzo']])
+            $statement->execute( [ $_GET['titolo'],  $_GET['autore'], $_GET['prezzo'] ] );
+            //$statement->execute( [ $_GET['titolo'],  $_GET['autore'], $_GET['prezzo'] ] );
+        */
+
+        //echo json_encode($effrazione);
+        //$statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e)
+        {
+            echo "non funziona";
+            sendResponse(300, $e->getMessage());
+        }
         
+
     }
 
     function elencaMulte()
     {
-       // echo "Hai chiesto di elencare le multe";
-
+       try
+       {
+            $matricola = $_POST['matricola'];
+            $miaQuery = "SELECT * FROM multe WHERE ID_Operatore = $matricola";
+            $statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
+            $risultati = $statement->fetchAll();
+            //echo $miaQuery;
+            sendResponse(200, json_encode($risultati), "application/json");
+       }
+       catch(PDOException $e)
+       {
+            sendResponse(500, $e->getMessage());
+       }
     }
 
     function annullaMulta()
     {
         //echo "Hai chiesto di annullare una multa";
+        //DELETE FROM table_name WHERE condition
+        $multadaCanc = $_POST['ID_Multa'];
+        $miaQuery = "DELETE from multe where ID_Multa = $multadaCanc";
+        try
+        {
+            $statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
+            $risultati = $statement->fetchAll();
+            //echo $miaQuery;
+            sendResponse(200, json_encode($risultati), "application/json");
+        }
+        catch(PDOException $e)
+        {
+            sendResponse(500, $e->getMessage());
+        }
+
     }
 
     function richiediSanzioni()
     {
         $miaQuery = "SELECT * FROM `tipoeffrazione`";
         $statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
-
-        $risultati = []; 
-
-        foreach($statement as $row)
-        {
-            $risultati [] = $row;
-        } 
+        $risultati = $statement->fetchAll();
         sendResponse(200, json_encode($risultati), "application/json");
     }
 
     function autenticazione()
     {
         global $autenticato;
-        
         try
         {
+            hash("md5","ciao");
             $token = $_POST['key'];
             $miaQuery = "SELECT Token from operatore where Token = '$token'";
             $statement = $this->$db_connection->query($miaQuery, PDO::FETCH_ASSOC);
-
+            /*
             $risultati = []; 
 
             foreach($statement as $row)
             {
                 $risultati [] = $row;
             } 
+            */
+            $risultati = $statement->fetchAll();
             //Il risultato della query è una matrice e mi interessa solo il primo valore del primo vettore           
             $vettoreTokenOttenuto = $risultati[0];
             //prendo il valore del token e lo metto in una variabile di supporto 
@@ -103,17 +179,12 @@ class serviceApi
             //se il risultato della query è vuoto allora il token o è scaduto o non è presente nel database
             if($tokenOttenuto == "")
             {
-                sendResponse(200, json_encode($risultati), "application/json");
-                sendResponse(200, "Non è stato possibile autenticarti", "application/json");
-                sendResponse(200, json_encode($autenticato), "application/json");
-
-
+                sendResponse(401, json_encode("Non è stato possibile autenticarti"), "application/json");
             }
             else{
-                echo "Sei stato autenticato";
-                sendResponse(200, json_encode($risultati), "application/json");
+                $body = ['risQuery' => $risultati, 'msg' =>  "Sei stato autenticato"];
+                //sendResponse(200, json_encode($body), "application/json");
                 $autenticato = true;
-                sendResponse(200, json_encode($autenticato), "application/json"); 
                 //da correggere qua la variabile lucchetto per l'autenticazione  
             }
         }
@@ -130,26 +201,24 @@ class serviceApi
 $api = new serviceApi();
 $nome_funzione = $_POST['function'];
 //router
-
 $api->autenticazione();
-sendResponse(200, json_encode($autenticato), "application/json");
 if($autenticato == true)
 {
     switch($nome_funzione)
     {
-        case 'nuova_multa':
-            $api->elencaMulte();
+        case 'nuova_multa'://quasi fatto manca passare più effrazioni
+            $api->nuovaMulta();
         break;
     
-        case 'elenco_multe':
+        case 'elenco_multe'://fatto
            $api->elencaMulte();
         break;
 
-        case 'annulla_multa':
-            $api->elencaMulte();
+        case 'annulla_multa'://quasi fatto manca cancellare più multe insieme
+            $api->annullaMulta();
         break;
     
-        case 'sanzioni_disponibili':
+        case 'sanzioni_disponibili'://fatto
             $api->richiediSanzioni();
         break;
     
